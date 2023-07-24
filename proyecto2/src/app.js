c77f2b3c38bc064c9ed29b21b4fa24acc63656e2
Lucas Pedemonti <lucaspedemonti91@gmail.com>
2023-07-23 21:55:20 -0300

    Websocket

--- proyecto2/src/app.js
+++ proyecto2/src/app.js
@@ -1,12 +1,25 @@
-import express from "express";
-import productRoutes from "./router/product.router.js";
-import cartRoutes from "./router/cart.router.js";
-import __dirname from "./utils.js";
-import { Server } from "socket.io";
 import { engine } from "express-handlebars";
+import express from "express";
+import { __filename, __dirname } from "./utils.js";
+import viewsRoutes from "./routes/views.router.js";
+import viewsRealTime from "./routes/realTimeProduct.router.js";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import { guardarProducto } from "./services/productUtils.js";
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const httpServer = createServer(app);
+
+const PORT = process.env.PORT || 8081;
+
+
+//APP ENGINE
+app.engine("handlebars", engine());//si tiene una funcion que tiene dos parentesis, significa ejecute el engine
+app.set("view engine", "handlebars");//Va a buscar los archivos con extension handlebars
+app.set("views", `${__dirname}/views`);//Que van a estar dentro del directorio src / views
+
+// Configurar el directorio estático para archivos públicos
+app.use(express.static("public"));
 
 // Configurar middlewares
 app
@@ -14,22 +27,52 @@ app
   .use(express.json()) // Parsear datos en formato JSON
   .use(express.static(`${__dirname}/public`)); // Servir archivos estáticos desde la carpeta "public"
 
-//APP ENGINE
-app.engine("handlebars", engine());//si tiene una funcion que tiene dos parentesis, significa ejecute el engine
-app.set("view enginw", "handlebars");//Va a buscar los archivos con extension handlebars
-app.set("views", "./src/views");//Que van a estar dentro del directorio src / views
 
 // Rutas
 app
-  .use("/api/products", productRoutes) // Rutas relacionadas con los productos
-  .use("/api/carts", cartRoutes); // Rutas relacionadas con los carritos
+  //.use("/api/products", productRoutes) // Rutas relacionadas con los productos
+  //.use("/api/carts", cartRoutes); // Rutas relacionadas con los carritos
+  app.use("/", viewsRoutes);
+  app.use("/realtime", viewsRealTime);
 
-// Iniciar el servidor
-const connectedServer = app.listen(PORT, () => {
-  console.log(`Servidor ejecutándose en el puerto: ${PORT}`);
+// Iniciar el servidor HTTP
+httpServer.listen(PORT, () => {
+  console.log(`Servidor en ejecución en http://localhost:${PORT}`);
 });
 
-// Manejar errores
-connectedServer.on("error", (error) => {
-  console.log(error.message);
+// Configuración del lado del servidor
+const io = new Server(httpServer);
+
+// Configurar el evento de conexión de Socket.IO
+io.on("connection", (socket) => {
+  console.log("Nuevo cliente conectado");
+
+  // Manejar eventos personalizados
+  socket.on("mensaje", (data) => {
+    console.log("Mensaje recibido:", data);
+
+    // Enviar una respuesta al cliente
+    socket.emit("respuesta", "Mensaje recibido correctamente");
+  });
+
+  // Escuchar evento 'agregarProducto' y emitir 'nuevoProductoAgregado'
+  socket.on("agregarProducto", (newProduct) => {
+    console.log("Nuevo producto recibido backend:", newProduct);
+    guardarProducto(newProduct);
+    // Agregar el nuevo producto a la lista de productos
+    io.emit("nuevoProductoAgregado", newProduct);
+  });
+
+  /*socket.on("productoEliminado", (productID) => {
+    // Eliminar el producto de la lista en el cliente
+    const productoElement = document.querySelector(`[data-id="${productID}"]`);
+    if (productoElement) {
+      productoElement.parentElement.remove();
+    }
+  });
+  */
+
+  socket.on("disconnect", () => {
+    console.log("Cliente desconectado");
+  });
 });
